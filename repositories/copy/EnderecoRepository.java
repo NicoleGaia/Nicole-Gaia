c10eb643481b576da087f63d35be942c2cd1d91b
NicoleGaia <86980102+NicoleGaia@users.noreply.github.com>
2021-11-17 14:28:18 -0300

    Spring

---
+++ repositories/copy/EnderecoRepository.java
@@ -0,0 +1,10 @@
+package com.senai.repositories.copy;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.domain.Endereco;
+
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
