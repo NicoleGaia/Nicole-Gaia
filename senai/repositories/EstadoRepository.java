3f1e8e8d7b36721f08ad06c5f6ce4aec67d46ed3
NicoleGaia <86980102+NicoleGaia@users.noreply.github.com>
2021-11-29 16:07:44 -0300

    Spring

---
+++ senai/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.senai.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.senai.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
