6c6f8d97ebb47c405bb8757e820e7ada705eda82
NicoleGaia <86980102+NicoleGaia@users.noreply.github.com>
2021-11-29 15:44:36 -0300

    delete

--- repositories/copy/EstadoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.repositories.copy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.domain.Estado;
-
-public interface EstadoRepository extends JpaRepository<Estado, Integer>{
-
-}
